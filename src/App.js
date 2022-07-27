ea7cd0e894e649f3e8e2f8c1fbc808fd016999b6
torvic <130111@unsaac.edu.pe>
2022-07-26 19:17:07 -0500

    Add tailwind css to the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </div>
 );
 
