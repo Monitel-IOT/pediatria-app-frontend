a5e8d86d312cf86abc0bcb94df38feda09b9b5d2
torvic <130111@unsaac.edu.pe>
2022-08-12 22:01:27 -0500

    Feat: Add base/strings file to the project

---
+++ src/base/strings.js
@@ -0,0 +1,11 @@
+export const LOGIN = {
+  title: 'Its Great to see you',
+  subtitle: 'Login Here',
+  button: 'Iniciar Sesión',
+};
+
+export const REGISTER = {
+  title: 'Create Your Account',
+  subtitle: 'Lets roll!',
+  button: 'Registrarse',
+};
