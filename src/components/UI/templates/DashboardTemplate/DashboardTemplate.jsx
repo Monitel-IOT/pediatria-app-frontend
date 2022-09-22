a7abba847e98d0b8467fec3c0eca249cb96af182
torvic <130111@unsaac.edu.pe>
2022-09-22 12:48:55 -0500

    Feat: comment console log in Dashboard Template

--- src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
+++ src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Wrapper from '../../../layout/Wrapper/Wrapper';
 
-const DashboardTemplate = () => {
-  const estado = useSelector((state) => state.charactersRickAndMortyReducer);
-  console.log(estado.loading);
-  return (
-    <Wrapper>
-      <div className="h-[calc(100vh-5rem)]">DashboardTemplae</div>
-    </Wrapper>
-  );
-};
+const DashboardTemplate = () => (
+  <Wrapper>
+    <div className="h-[calc(100vh-5rem)]">DashboardTemplae</div>
+  </Wrapper>
+);
 
 export default DashboardTemplate;
