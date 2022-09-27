adf6601ca7bf9a6f6bfba8f3995ff8373874ec46
torvic <130111@unsaac.edu.pe>
2022-09-27 15:51:14 -0500

    Feat: Add validation to LoginForm component

--- src/components/UI/atoms/RouteLink/RouteLink.jsx
+++ src/components/UI/atoms/RouteLink/RouteLink.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const RouteLink = ({ children, ...props }) => (
-  <Link {...props} className="text-sm uppercase text-blue-main-500">{children}</Link>
+  <Link {...props} className="text-xs sm:text-sm uppercase text-blue-main-500">{children}</Link>
 );
 
 RouteLink.propTypes = {
