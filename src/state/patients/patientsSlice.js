bb6446fb021d63e4659ed19fc99decaf78cd42f8
Cristian Benites Condori <78431820+cb3ndev@users.noreply.github.com>
2022-09-29 23:05:07 -0500

    fix: Integration paginator & search

--- src/state/patients/patientsSlice.js
+++ src/state/patients/patientsSlice.js
@@ -1,16 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchPatients, addNewPatient } from '../../thunkAction/patients/patientsThunk';
-import { sortLists, flatByPages } from '../../utils';
+import { sortLists, flatByPages, filterSearch } from '../../utils';
 
 const initialState = {
-  patients: [],
+  results: [],
   status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
-  patientsFiltered: [],
+  patients: [],
   resultsByPage: [],
   sortedResults: [],
   page: 0,
   toggleSort: true,
+  filterResults: [],
   // newPatient:{},
 };
 
@@ -18,8 +19,14 @@ const patientsSlice = createSlice({
   name: 'patients',
   initialState,
   reducers: {
+    filterBy: (state, action) => {
+      const filteredPatients = filterSearch(state.patients, action.payload[0], action.payload[1]);
+      const pages = flatByPages(filteredPatients, 4);
+      state.filterResults = filteredPatients;
+      state.resultsByPage = pages;
+    },
     orderById: (state) => {
-      const sortedResults = sortLists('nombre', state.patientsFiltered, state.toggleSort);
+      const sortedResults = sortLists('nombre', state.patients, state.toggleSort);
       const pages = flatByPages(sortedResults, 4);
       state.toggleSort = !state.toggleSort;
       state.sortedResults = sortedResults;
@@ -39,7 +46,7 @@ const patientsSlice = createSlice({
       })
       .addCase(fetchPatients.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.patients = action.payload.data;
+        state.results = action.payload.data;
         // const newArray = action.payload.data.map(({ DNI, Name, ...keepAttrs }) => keepAttrs);
         // console.log(newArray);
         const newArray = action.payload.data.map((patient) => ({
@@ -49,8 +56,9 @@ const patientsSlice = createSlice({
           fechaNacimiento: patient.DateBirth,
         }));
 
-        state.patientsFiltered = newArray;
+        state.patients = newArray;
         state.sortedResults = newArray;
+        state.filterResults = newArray;
         state.resultsByPage = flatByPages(newArray, 4);
       })
       .addCase(fetchPatients.rejected, (state, action) => {
@@ -63,7 +71,7 @@ const patientsSlice = createSlice({
       })
       .addCase(addNewPatient.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.patients.push(action.payload);
+        state.results.push(action.payload);
       })
       .addCase(addNewPatient.rejected, (state, action) => {
         state.status = 'failed';
@@ -72,15 +80,15 @@ const patientsSlice = createSlice({
   },
 });
 
-export const getAllPatientsOfADoctor = (state) => state.patients.patients;
-export const getPatientsStatus = (state) => state.patients.status;
-export const getPatientsError = (state) => state.patients.error;
+export const getAllPatientsOfADoctor = (state) => state.results.patients;
+export const getPatientsStatus = (state) => state.results.status;
+export const getPatientsError = (state) => state.results.error;
 
 export const getPatientByDni = (state, dni) => {
-  state.patients.patients.find((patient) => patient.DNI === dni);
+  state.results.patients.find((patient) => patient.DNI === dni);
 };
 
 export const {
-  orderById, changePage,
+  orderById, changePage, filterBy,
 } = patientsSlice.actions;
 export default patientsSlice.reducer;
