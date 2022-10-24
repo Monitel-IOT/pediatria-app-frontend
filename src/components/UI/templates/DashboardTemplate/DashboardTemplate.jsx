a777c7838888d9ee1845eecf525e4fa841c522b5
Cristian Benites Condori <78431820+cb3ndev@users.noreply.github.com>
2022-10-24 11:09:07 -0500

    feat: integracion update appointment
    ejemplo en dashboard view

--- src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
+++ src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
@@ -7,6 +7,7 @@ import { fetchPatientsById } from '../../../../thunkAction/patients/patientsThun
 import Button from '../../atoms/Button/Button';
 import Measurement from '../../molecules/Measurement/Measurement';
 import Container from '../../../layout/Container/Container';
+import { updateAppointmentByIdAPI } from '../../../../thunkAction/appointments/appointmentsThunk';
 
 const DashboardTemplate = () => {
   const dispatch = useDispatch();
@@ -18,11 +19,85 @@ const DashboardTemplate = () => {
   // } = useSelector((state) => state.authReducer);
 
   // console.log(user);
+  const appointmentObject = {
+    idAppointment: '6351eb032e655955f5166c2d',
+    newAppointment: {
+      characteristicsAttention: {
+      },
+      sickTime: '400 dias',
+      vitalFunctions: {
+        fr: 37,
+        fc: 15,
+        t: 36,
+        saturacion: 95,
+      },
+      percentiles: {
+        percentileWeight: 37,
+        heightPercentile: 15,
+        percentilMC: 36,
+      },
+      appointmentDate: '2023-08-18T00:00:00.000Z',
+      weight: 5.78,
+      size: 78,
+      age: '2 años',
+      brainPerimeter: 15,
+      bodyMassIndex: 5,
+      prolongedDiagnosis: [
+        {
+          diagnosis: 'Diabetes',
+          age: '3 meses',
+        },
+        {
+          diagnosis: 'No se',
+          age: '2 meses',
+        },
+      ],
+      diagnosis: [
+        {
+          diagnosticName: 'Gripe',
+        },
+        {
+          diagnosticName: 'Infeccion estomacal',
+        },
+      ],
+      longTreatment: [
+        {
+          longTreatmentName: 'Eutirox',
+          suspensionDate: null,
+        },
+        {
+          longTreatmentName: 'Ranttt',
+          suspensionDate: null,
+        },
+      ],
+      treatment: [
+        {
+          nameTreatment: 'Ibuprofeno',
+        },
+        {
+          nameTreatment: 'Paracetamol',
+        },
+      ],
+      auxiliaryExams: [
+        {
+          exams: 'Orina',
+        },
+        {
+          exams: 'Sangre',
+        },
+      ],
+      state: true,
+    },
+  };
 
   return (
     <Wrapper>
       <Container className="h-[calc(100vh-5rem)]">
         <h1>Dashboard Template</h1>
+        <Button primary onClick={() => dispatch(updateAppointmentByIdAPI(appointmentObject))}>
+          CLICK actualizar appointment con id:
+          {` ${appointmentObject.idAppointment}`}
+        </Button>
         <Button primary onClick={() => dispatch(fetchPatientsById('630eef31b204ef57c21ed3d3'))}>
           CLICK para cargar un paciente
         </Button>
