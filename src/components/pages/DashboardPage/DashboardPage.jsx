092986ac85959597560c88ce4cc9f65de45e847a
torvic <130111@unsaac.edu.pe>
2022-10-24 17:01:53 -0500

    Feat: Add Alert component & use it in Dashboard page

--- src/components/pages/DashboardPage/DashboardPage.jsx
+++ src/components/pages/DashboardPage/DashboardPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import DashboardTemplae from '../../UI/templates/DashboardTemplate/DashboardTemplate';
+import DashboardTemplate from '../../UI/templates/DashboardTemplate/DashboardTemplate';
 
 const DashboardPage = () => (
-  <DashboardTemplae />
+  <DashboardTemplate />
 );
 
 export default DashboardPage;
