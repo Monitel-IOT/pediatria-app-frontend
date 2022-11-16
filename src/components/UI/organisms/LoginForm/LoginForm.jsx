1efb9216bf373e0c44ef639f38aa1ec0e6c8055d
torvic <torvic02st96@gmail.com>
2022-11-15 19:07:35 -0500

    Feat: Add loading icon & use it in login view

--- src/components/UI/organisms/LoginForm/LoginForm.jsx
+++ src/components/UI/organisms/LoginForm/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { handleChange, initialStateForm } from '../../../../state/loginForm/loginFormSlice';
 import { signInWithEmailAndPassword } from '../../../../thunkAction/auth/authThunk';
 import Button from '../../atoms/Button/Button';
@@ -34,53 +36,63 @@ const LoginForm = () => {
     navigate('/');
     dispatch(initialStateForm());
   };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <FontAwesomeIcon icon={faCog} className="fa-spin text-[4rem] text-blue-main-500" />
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-gray-100 m-auto sm:max-w-md pt-40 sm:pt-32 h-screen">
-      <Typography component="h2" className="uppercase font-bold">
-        Es genial verte!
-      </Typography>
-      <Typography component="h4" className="uppercase text-cyan-600">
-        Ingrese Aquí
-      </Typography>
-      <Card className="mt-4">
-        <form onSubmit={handleSubmit}>
-          <FormInput
-            label="Email"
-            type="email"
-            placeholder="example@example.com"
-            name="email"
-            value={form.email}
-            onChange={(e) => dispatch(handleChange(
-              { name: e.target.name, value: e.target.value },
-            ))}
-            error={errors.email}
-          />
-          <FormInput
-            label="Contraseña"
-            type="password"
-            placeholder="Contraseña"
-            name="password"
-            value={form.password}
-            onChange={(e) => dispatch(handleChange(
-              { name: e.target.name, value: e.target.value },
-            ))}
-            error={errors.password}
-          />
-          { loading && <div className="pb-2">Cargando...</div> }
-          { error && <div className="text-red-500 pb-2">{error.code}</div> }
-          { location?.state?.message && <div className="text-green-500 pb-2">{location.state.message}</div> }
-          <div className="flex items-center justify-between flex-wrap">
-            <RouteLink to="/forgotPassword">¿Olvidaste tu contraseña?</RouteLink>
-            <Button
-              primary={!isDisabled}
-              disabled={isDisabled}
-              type="submit"
-            >
-              Iniciar Sesión
-            </Button>
-          </div>
-        </form>
-      </Card>
+    <div className=" bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-full sm:w-[30rem]">
+        <Typography component="h2" className="uppercase font-bold">
+          Es genial verte!
+        </Typography>
+        <Typography component="h4" className="uppercase text-cyan-600">
+          Ingrese Aquí
+        </Typography>
+        <Card className="mt-4">
+          <form onSubmit={handleSubmit}>
+            <FormInput
+              label="Email"
+              type="email"
+              placeholder="example@example.com"
+              name="email"
+              value={form.email}
+              onChange={(e) => dispatch(handleChange(
+                { name: e.target.name, value: e.target.value },
+              ))}
+              error={errors.email}
+            />
+            <FormInput
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              value={form.password}
+              onChange={(e) => dispatch(handleChange(
+                { name: e.target.name, value: e.target.value },
+              ))}
+              error={errors.password}
+            />
+            { error && <div className="text-red-500 pb-2">{error.code}</div> }
+            { location?.state?.message && <div className="text-green-500 pb-2">{location.state.message}</div> }
+            <div className="flex items-center justify-between flex-wrap">
+              <RouteLink to="/forgotPassword">¿Olvidaste tu contraseña?</RouteLink>
+              <Button
+                primary={!isDisabled}
+                disabled={isDisabled}
+                type="submit"
+              >
+                Iniciar Sesión
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
