1b5e2e8b2b5190be818d569daa97a328862bb040
torvic <torvic02st96@gmail.com>
2022-11-15 19:07:35 -0500

    Feat: Add loading icon & use it in login view

--- src/components/UI/organisms/RegisterForm/RegisterForm.jsx
+++ src/components/UI/organisms/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { handleChange, initialStateForm } from '../../../../state/registerForm/registerFormSlice';
 import { signUpWithEmailAndPassword } from '../../../../thunkAction/auth/authThunk';
 import Button from '../../atoms/Button/Button';
@@ -29,62 +31,72 @@ const RegisterForm = () => {
       });
   };
 
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <FontAwesomeIcon icon={faCog} className="fa-spin text-[4rem] text-blue-main-500" />
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-gray-100 sm:max-w-md pt-28 m-auto h-screen">
-      <Typography component="h2" className="uppercase font-bold">
-        Crea tu cuenta
-      </Typography>
-      <Typography component="h4" className="uppercase text-cyan-600">
-        {'Let\'s Roll'}
-      </Typography>
-      <Card className="mt-4">
-        <form onSubmit={handleSubmit}>
-          <FormInput
-            label="Nombres"
-            type="text"
-            placeholder="Nombres"
-            name="name"
-            value={form.name}
-            onChange={(e) => dispatch(handleChange(
-              { name: e.target.name, value: e.target.value },
-            ))}
-            error={errors.name}
-          />
-          <FormInput
-            label="Email"
-            type="email"
-            placeholder="example@example.com"
-            name="email"
-            value={form.email}
-            onChange={(e) => dispatch(handleChange(
-              { name: e.target.name, value: e.target.value },
-            ))}
-            error={errors.email}
-          />
-          <FormInput
-            label="Contraseña"
-            type="password"
-            placeholder="Contraseña"
-            name="password"
-            value={form.password}
-            onChange={(e) => dispatch(handleChange(
-              { name: e.target.name, value: e.target.value },
-            ))}
-            error={errors.password}
-          />
-          { loading && <div className="pb-2">Cargando...</div> }
-          { error && <div className="text-red-500 pb-2">{error.code}</div> }
-          <Stack horizontal="right">
-            <Button
-              primary={!isDisabled}
-              disabled={isDisabled}
-              type="submit"
-            >
-              Registrarse
-            </Button>
-          </Stack>
-        </form>
-      </Card>
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-full sm:w-[30rem]">
+        <Typography component="h2" className="uppercase font-bold">
+          Crea tu cuenta
+        </Typography>
+        <Typography component="h4" className="uppercase text-cyan-600">
+          {'Let\'s Roll'}
+        </Typography>
+        <Card className="mt-4">
+          <form onSubmit={handleSubmit}>
+            <FormInput
+              label="Nombres"
+              type="text"
+              placeholder="Nombres"
+              name="name"
+              value={form.name}
+              onChange={(e) => dispatch(handleChange(
+                { name: e.target.name, value: e.target.value },
+              ))}
+              error={errors.name}
+            />
+            <FormInput
+              label="Email"
+              type="email"
+              placeholder="example@example.com"
+              name="email"
+              value={form.email}
+              onChange={(e) => dispatch(handleChange(
+                { name: e.target.name, value: e.target.value },
+              ))}
+              error={errors.email}
+            />
+            <FormInput
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              value={form.password}
+              onChange={(e) => dispatch(handleChange(
+                { name: e.target.name, value: e.target.value },
+              ))}
+              error={errors.password}
+            />
+            { loading && <div className="pb-2">Cargando...</div> }
+            { error && <div className="text-red-500 pb-2">{error.code}</div> }
+            <Stack horizontal="right">
+              <Button
+                primary={!isDisabled}
+                disabled={isDisabled}
+                type="submit"
+              >
+                Registrarse
+              </Button>
+            </Stack>
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
