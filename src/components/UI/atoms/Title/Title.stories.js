49749a281bf9c9288fe602892f805edeb1a4826d
torvic <torvic02st96@gmail.com>
2022-11-18 15:05:43 -0500

    Feat: Add storybook to the project

--- src/components/UI/atoms/Title/Title.stories.js
+++
@@ -1,8 +0,0 @@
-import TitleAtom from "./UI/atoms/TitleAtom";
-
-export default {
-  title: "TitleAtom",
-  component:TitleAtom,
-}
-
-export const Template = () => <TitleAtom text="ThisIsATitle" color="blue-main" size="2xl"/>
