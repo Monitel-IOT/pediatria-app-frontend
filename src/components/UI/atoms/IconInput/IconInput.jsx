a336b3833318e8e2b4c3f35d63afdee6a272b258
torvic <torvic02st96@gmail.com>
2022-12-12 11:05:55 -0500

    Feat: Add button new patient in the patient page

--- src/components/UI/atoms/IconInput/IconInput.jsx
+++ src/components/UI/atoms/IconInput/IconInput.jsx
@@ -6,18 +6,18 @@ const IconInput = ({
 }) => (
   <div className={`flex border border-gray-300 rounded-full focus-within:outline focus-within:outline-2 focus-within:outline-blue-main-500 overflow-hidden ${props.className}`}>
     {iconLeft && (
-    <label htmlFor="email" className="flex items-center pl-2 bg-white">
-      <span className="text-gray-500">{iconLeft}</span>
+    <label htmlFor="email" className="flex items-center pl-5 bg-white">
+      <span className="text-gray-300">{iconLeft}</span>
     </label>
     ) }
     <input
       id="email"
       {...props}
-      className="px-2 py-2 border-0 focus:outline-none w-full"
+      className="px-5 py-2 border-0 focus:outline-none w-full"
     />
     {iconRigth && (
-    <label htmlFor="email" className="flex items-center pr-4 bg-white">
-      <span className="text-gray-500">{iconRigth}</span>
+    <label htmlFor="email" className="flex items-center pr-5 bg-white">
+      <span className="text-gray-300">{iconRigth}</span>
     </label>
     ) }
   </div>
