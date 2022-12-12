110b6a329c8bf8a66bc1b40543afa2a917dc7357
torvic <torvic02st96@gmail.com>
2022-12-12 11:57:40 -0500

    feat: add button edit & delete in patient list page

--- src/components/UI/atoms/Td/Td.jsx
+++ src/components/UI/atoms/Td/Td.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Td = ({ children }) => (
-  <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">{children}</td>
+const Td = ({ children, ...props }) => (
+  <td {...props} className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">{children}</td>
 );
 
 Td.propTypes = {
