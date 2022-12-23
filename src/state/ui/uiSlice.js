5b56cfade3e0f9602b6a54d0d446286e80db7fa0
miriacode <miriamacunaenciso@gmail.com>
2022-12-23 00:10:34 -0500

    feat: login alerts

--- src/state/ui/uiSlice.js
+++ src/state/ui/uiSlice.js
@@ -2,6 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   showResponseModal: false,
+  showLoginErrorAlert: false,
+  showLoginSuccessAlert: false,
 };
 
 const uiSlice = createSlice({
@@ -14,11 +16,27 @@ const uiSlice = createSlice({
     closeResponseModal: (state) => {
       state.showResponseModal = false;
     },
+    openLoginErrorAlert: (state) => {
+      state.showLoginErrorAlert = true;
+    },
+    closeLoginErrorAlert: (state) => {
+      state.showLoginErrorAlert = false;
+    },
+    openLoginSuccessAlert: (state) => {
+      state.showLoginSuccessAlert = true;
+    },
+    closeLoginSuccessAlert: (state) => {
+      state.showLoginSuccessAlert = false;
+    },
   },
 });
 
 export const {
   openResponseModal,
   closeResponseModal,
+  openLoginErrorAlert,
+  closeLoginErrorAlert,
+  openLoginSuccessAlert,
+  closeLoginSuccessAlert,
 } = uiSlice.actions;
 export default uiSlice.reducer;
