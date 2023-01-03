1d21bc10c5bd3736eb153d9f217fac49e224f665
HenryCodeT <120686@unsaac.edu.pe>
2023-01-02 19:14:18 -0500

    añadiendo nueva libreria para los graficos, añadiendo dashboard

--- src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
+++ src/components/UI/templates/DashboardTemplate/DashboardTemplate.jsx
@@ -1,30 +1,78 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+// eslint-disable-next-line import/no-unresolved
+import { Bar } from 'react-chartjs-2';
 import Wrapper from '../../../layout/Wrapper/Wrapper';
-import Measurement from '../../molecules/Measurement/Measurement';
 import Container from '../../../layout/Container/Container';
 import PageHeader from '../../organisms/PageHeader/PageHeader';
 
-const DashboardTemplate = () => {
-  // const {
-  //   user,
-  // } = useSelector((state) => state.authReducer);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Atenciones por mes',
+    },
+  },
+};
+const labels = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Atenciones',
+      data: [10, 22, 33, 44, 55, 34, 34, 34, 67, 100, 120, 43],
+      backgroundColor: 'rgb(2, 132, 199)',
+    },
+  ],
+};
+
+const DashboardTemplate = () => {
   const dataPages = [
     {
       link: '/dashboard',
       text: 'Dashboard',
     }];
-  // console.log(user);
 
   return (
     <Wrapper>
       <Container className="py-4">
         <PageHeader title="Dashboard" dataPages={dataPages} />
         <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-5 md:grid-cols-4">
-          <Measurement icon={<FontAwesomeIcon icon={faUsers} />} text="Pacientes" value="20" />
-          <Measurement icon={<FontAwesomeIcon icon={faCalendarCheck} />} text="Atenciones" value="14" />
+          <Bar options={options} data={data} />
         </div>
       </Container>
     </Wrapper>
